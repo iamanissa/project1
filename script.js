105f55641486b53279ec6fd23f1b43a370c928e1
Anissa Jones <anissajones10@gmail.com>
2016-02-22 13:55:12 -0500

    Hard coded colors by using repeat event listeners and functions.

--- script.js
+++ script.js
@@ -1,15 +1,28 @@
-var grid = getElementByTagName("tbody");
-var eachCard = getElementsByTagName('td');
+var cardOne = document.querySelector(".card1");
+var cardTwo = document.querySelector(".card2");
+var cardThree = document.querySelector(".card3");
+var cardFour = document.querySelector(".card4");
 
-grid.addEventListener("click",colorToRed);
 
+cardOne.addEventListener("click",colorToRed);
+cardTwo.addEventListener("click",colorToBLue);
+cardThree.addEventListener("click",colorToRed2);
+cardFour.addEventListener("click",colorToBLue2);
+
+
+function colorToRed(){
+  cardOne.style.backgroundColor = "red";
+}
 function colorToBLue(){
-  grid.style.backgroundColor = "blue";
+  cardTwo.style.backgroundColor = "blue";
 }
-function colorToRed(e){
-  if(e.target && e.target.nodeName == "td"){
-  eachCard.style.backgroundColor = "red";
-  }
+
+function colorToRed2(){
+  cardThree.style.backgroundColor = "red";
 }
+function colorToBLue2(){
+  cardFour.style.backgroundColor = "blue";
+}
+
 //hard code colors first
 //after, how to randomizing adding classes to div(td)
