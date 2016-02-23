e81a2a0a7f6f8618f616ce289f50d7a81d510bc0
Anissa Jones <anissajones10@gmail.com>
2016-02-23 12:39:25 -0500

    Trying to incorporate a tracker 'itMatches'.

--- script.js
+++ script.js
@@ -1,39 +1,28 @@
+var itMatches = false;
 var cards = document.querySelectorAll("[data-color]");
 
 
 for(i=0;i<cards.length; i++){
-  cards[i].addEventListener("click", changeToColor);
-}
-
-
-//what do you want when you click the div(card)? change color.
-function changeToColor(){
-  this.getAttribute("data-color");
-  this.classList.add("active");
-  stopAfterTwoCards();
-  compare();
-}
-
-//only have 2 active cards at a time.
-function stopAfterTwoCards(e){
-  activeArray = document.querySelectorAll(".active");
-  if(activeArray.length === 2){
-    for(i=2;i<cards.length; i++){
-      cards[i].addEventListener("click", function backToGray(){
-        this.className += " grayAgain";
-      });
+  cards[i].addEventListener("click", function(){
+    var currentCard = this;
+    activeArray = document.querySelectorAll(".active");
+    if(!itMatches){
+      this.classList.add("active");
+      if(activeArray.length === 2){
+        if(activeArray[0].getAttribute("data-color") === activeArray[1].getAttribute("data-color")){
+          itMatches = true;
+          console.log("These cards are the same");
+        }else{
+          itMatches = false;
+          console.log("these cards are different");
+        }
+      }
     }
-  }
+
+  });
 }
 
-function compare(){
-  activeArray = document.querySelectorAll(".active");
-  if(activeArray.length === 2){
-    if(activeArray[0].getAttribute("data-color") === activeArray[1].getAttribute("data-color")){
-      console.log("These cards are the same");
-    }
-  }
-}
+
 
 //what does it mean to flip the cards over?
 //if active cards are the same, do something, like remove them.
