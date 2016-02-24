efaafe3e67500d5e92e9a9141e3b0c723db62875
Anissa Jones <anissajones10@gmail.com>
2016-02-24 12:26:04 -0500

    added a reset function to reset board and attached it to the reset game button.

--- script.js
+++ script.js
@@ -1,78 +1,86 @@
 
-arrayOfClasses = ["red", "red", "blue", "blue"];
+var arrayOfClasses = ["red", "red", "blue", "blue"];
 var newDeck = shuffleDeck(arrayOfClasses);
 var board1 = document.querySelector("#board");
 
-function resetDeck(){
 
-  return newDeck;
+function resetBoard(){
+  win = document.querySelector(".win");
+  win.classList.add("hidden");
+  parent = document.getElementById("board");
+  allCards = document.querySelectorAll(".color");
+  for(i=0;i<allCards.length; i++){
+      parent.removeChild(allCards[i]);
+  }
+  makeBoard();
 }
 
-for(i=0;i<newDeck.length; i++){
-  var div = document.createElement("div");
-  div.classList.add("color");
-  board1.appendChild(div);
-  div.setAttribute("data-color", newDeck[i]);
-}
+function makeBoard(){
+  for(i=0;i<newDeck.length; i++){
+    var div = document.createElement("div");
+    div.classList.add("color");
+    board1.appendChild(div);
+    div.setAttribute("data-color", newDeck[i]);
+  }
+  var cards = document.querySelectorAll("[data-color]");
+  // var startingDeck = ["dog", "dog", "rabbit", "rabbit"];
 
-var cards = document.querySelectorAll("[data-color]");
-// var startingDeck = ["dog", "dog", "rabbit", "rabbit"];
 
+  for(i=0;i<cards.length; i++){
+    cards[i].addEventListener("click", function(){
+      var currentCard = this;
+      currentCard.classList.add("active");
+      activeArray = document.querySelectorAll(".active");
 
-for(i=0;i<cards.length; i++){
-  cards[i].addEventListener("click", function(){
-    var currentCard = this;
-    currentCard.classList.add("active");
-    activeArray = document.querySelectorAll(".active");
+      if(activeArray.length === 2){
+        console.log(activeArray);
+        if(activeArray[0].getAttribute("data-color") === activeArray[1].getAttribute("data-color")){
+          console.log("These cards are the same");
+          yesMatch();
+          gameWon();
+        }else{
+          console.log("these cards are different");
+          noMatch();
+        }
+      }
+    });
+  }
 
-    if(activeArray.length === 2){
-      console.log(activeArray);
-      if(activeArray[0].getAttribute("data-color") === activeArray[1].getAttribute("data-color")){
-        console.log("These cards are the same");
-        yesMatch();
-        gameWon();
-      }else{
-        console.log("these cards are different");
-        noMatch();
+  function noMatch(){
+    for(i=0;i<cards.length; i++){
+      // if visible is set remove it, otherwise add it
+      if(cards[i].classList.contains("active")){
+        flipCard = cards[i];
+        setTimeout(function(){
+          this.classList.remove("active");
+        }.bind(flipCard), 250);
       }
     }
-  });
-}
+  }
 
-function noMatch(){
-  for(i=0;i<cards.length; i++){
-    // if visible is set remove it, otherwise add it
-    if(cards[i].classList.contains("active")){
-      flipCard = cards[i];
-      setTimeout(function(){
-        this.classList.remove("active");
-      }.bind(flipCard), 1000);
+  function yesMatch(){
+    for(i=0;i<cards.length; i++){
+      if(cards[i].classList.contains("active")){
+        keepCard = cards[i];
+        keepCard.classList.add("grayAgain");
+        setTimeout(function(){
+          this.classList.remove("active");
+        }.bind(keepCard), 250);
+      }
     }
   }
-}
 
-function yesMatch(){
-  for(i=0;i<cards.length; i++){
-    if(cards[i].classList.contains("active")){
-      keepCard = cards[i];
-      keepCard.classList.add("grayAgain");
-      setTimeout(function(){
-        this.classList.remove("active");
-      }.bind(keepCard), 1000);
-    }
-  }
-}
-
-function gameWon(){
-  var playerWin = 0;
-  for(i=0;i<cards.length; i++){
-    if(cards[i].classList.contains("grayAgain")){
-      win = document.querySelector(".win");
-      playerWin += 1;
-      if(playerWin === document.querySelectorAll("[data-color]").length){
-        console.log("You Win!");
-        win.classList.remove("hidden");
-        // win.classList.add("visible");
+  function gameWon(){
+    var playerWin = 0;
+    for(i=0;i<cards.length; i++){
+      if(cards[i].classList.contains("grayAgain")){
+        win = document.querySelector(".win");
+        playerWin += 1;
+        if(playerWin === document.querySelectorAll("[data-color]").length){
+          console.log("You Win!");
+          win.classList.remove("hidden");
+          // win.classList.add("visible");
+        }
       }
     }
   }
@@ -92,13 +100,11 @@ function shuffleDeck(array){
   return array;
 }
 
+document.getElementById("reset").addEventListener("click", resetBoard);
 
 
-
-//FIXME: add two classes, 'animal' & 'unique animal'
-
-//TODO: turn cards into grid.
-//TODO: Maybe make console.logs appear at top for user to see
-//TODO: silver: add pictures?
 //TODO: gold: how to reset without refreshing the page?
-//TODO: gold: Does the start button do anything?
+//TODO: silver: add pictures?
+//TODO: gold: Apply object oriented principles.
+
+//TODO = make arrayOfClasses2 = ["red", "red", "blue", "blue", "hotpink", "hotpink", "purple", "purple", "orangered", "orangered", "gold", "gold", "teal", "teal", "olive", "olive"];
