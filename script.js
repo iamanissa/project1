c24de040f8e696f885252e32ac19605c2acc2065
Anissa Jones <anissajones10@gmail.com>
2016-02-23 22:51:41 -0500

    Need to fix shuffle function or reset function.

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 var itMatches = false;
 var cards = document.querySelectorAll("[data-color]");
-
+var startingDeck = ["dog", "dog", "rabbit", "rabbit"];
 
 for(i=0;i<cards.length; i++){
   cards[i].addEventListener("click", function(){
@@ -50,20 +50,40 @@ function gameWon(){
   var playerWin = 0;
   for(i=0;i<cards.length; i++){
     if(cards[i].classList.contains("grayAgain")){
-      win = document.querySelector(".win")
+      win = document.querySelector(".win");
       playerWin += 1;
       if(playerWin === document.querySelectorAll("[data-color]").length){
         console.log("You Win!");
         win.classList.remove("hidden");
-        win.classList.add("visible");
+        // win.classList.add("visible");
       }
     }
   }
 }
 
 
-//TODO: what happens when you win?
-//TODO: add shuffle function.
+//found shuffle array here: https://www.frankmitchell.org/2015/01/fisher-yates/
+function shuffleDeck(array){
+  var i = 0, j = 0, temp = null;
+
+  for (i = array.length - 1; i > 0; i -= 1) {
+    j = Math.floor(Math.random() * (i + 1));
+    temp = array[i];
+    array[i] = array[j];
+    array[j] = temp;
+  }
+  return array;
+}
+
+function resetDeck(){
+  var newDeck = shuffleDeck(startingDeck);
+  for(i=0; i<newDeck.length; i++){
+    cards[i].classList.add(newDeck[i]);
+  }
+}
+
+
+//FIXME: how to add shuffled deck classes to cards?
 
 //TODO: turn cards into grid.
 //TODO: Maybe make console.logs appear at top for user to see
